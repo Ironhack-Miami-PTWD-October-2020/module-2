a0eaf277e5ec2d7aa0cbf1ad54503d3a20c944a7
Marcos <mp2772@gmail.com>
2021-01-04 21:54:53 -0500

    spotify lab search and search results review done (many did not complete so did not do full review)

--- week-2/get-post-example/app.js
+++ week-2/get-post-example/app.js
@@ -52,6 +52,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 app.use('/', index);
+// same as above 
+// app.use('/', require('./routes/index'));
 
 
 module.exports = app;
