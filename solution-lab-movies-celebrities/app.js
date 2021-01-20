375744cc979a43c9030c9943f6aca6cdcbc14822
sandrabosk <sandraboskovic@hotmail.com>
2021-01-20 14:32:32 +0100

    add solution to movies and celebrities-full crud

---
+++ solution-lab-movies-celebrities/app.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const logger = require("morgan");
+const path = require("path");
+
+// require database configuration
+require("./configs/db.config");
+
+const app_name = require("./package.json").name;
+const debug = require("debug")(`${app_name}:${path.basename(__filename).split(".")[0]}`);
+
+const app = express();
+
+// Middleware Setup
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express View engine setup
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+
+// default value for title local
+app.locals.title = "Express - Generated with IronGenerator";
+
+const index = require("./routes/index");
+app.use("/", index);
+
+app.use("/celebrities", require("./routes/celebrity.routes"));
+
+// the longer way:
+// const movieRoutes = require('./routes/movie.routes');
+// app.use('/movies', movieRoutes);
+
+// the shorter way of this above ^^^^^
+app.use("/movies", require("./routes/movie.routes"));
+
+module.exports = app;
